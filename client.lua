41f1ac121f7627df1dfd14b328d24be55f6fe693
RH_Adv1c3 <cgknappers@gmail.com>
2025-03-23 03:30:38 +0100

    Modernized code, added redlight, optimized usage

--- client.lua
+++ client.lua
@@ -3,44 +3,34 @@
 --------------------------------------
 
 --- Config ---
-
-activateWay = 0 -- 0 = use command, 1 = use keybind, 2 = use both command and keybind
-keybindKey = 182 -- http://docs.fivem.net/game-references/controls/
-intCommand = "il" -- If using the command, what do you want the command to be?
-
-
-
+local intCommand = "il" -- If using the command, what do you want the command to be?
+local redintlight = true -- If the interior light should be red light (Used by cops usually at night for better eye adaption)
 --- Code Shit ---
 
-function toggleInteriorLights(ped, veh)
-    if IsPedInVehicle(ped, veh, false) then
+--- Micro Opt --
+local SetVehicleInteriorlight = SetVehicleInteriorlight
+local IsVehicleInteriorLightOn = IsVehicleInteriorLightOn
+local IsPedInVehicle = IsPedInVehicle
+local GetVehiclePedIsUsing = GetVehiclePedIsUsing
+local SetVehicleFlag = SetVehicleFlag
+
+local function toggleInteriorLights(ped,veh)
+    if IsPedInVehicle(ped,veh,false) then
         if IsVehicleInteriorLightOn(veh) then
-            SetVehicleInteriorlight(veh, false)
+            if redintlight then SetVehicleFlag(veh,159,false) end
+            SetVehicleInteriorlight(veh,false)
         else
-            SetVehicleInteriorlight(veh, true)
+            if redintlight then SetVehicleFlag(veh,159,true) end
+            SetVehicleInteriorlight(veh,true)
         end
     else
         TriggerEvent("chat:addMessage", "^1^*You are not in a vehicle.")
     end
 end
 
-Citizen.CreateThread(function()
-	while true do
-        Citizen.Wait(0)
-        local ped = GetPlayerPed(PlayerId())
-        local veh = GetVehiclePedIsUsing(ped)
-        if activateWay == 1 or activateWay == 2 then
-            if IsControlJustPressed(1, keybindKey) then
-                toggleInteriorLights(ped, veh)
-            end
-        end
-	end
-end)
-
-RegisterCommand(intCommand, function(source, args, raw)
-    local ped = GetPlayerPed(PlayerId())
+RegisterCommand(intCommand,function()
+    local ped = PlayerPedId()
     local veh = GetVehiclePedIsUsing(ped)
-    if activateWay == 0 or activateWay == 2 then
-        toggleInteriorLights(ped, veh)
-    end
+    toggleInteriorLights(ped,veh)
 end)
+RegisterKeyMapping(intCommand,'Toggle Interior Light','KEYBOARD','L')
